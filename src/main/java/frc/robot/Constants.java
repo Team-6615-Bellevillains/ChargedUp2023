d84902c6c9ad94ac9300d1b89250fc2d4f04a29c
Ethan Hardy <12780774-ethanhardy@users.noreply.gitlab.com>
2023-02-03 18:41:41 -0500

    update ps4 controller constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -69,11 +69,11 @@ public final class Constants {
 
     public static final class OIConstants {
         public static final double kDeadband = 0.06;
-        public static final int ps4ControllerPort = 0;
+        public static final int ps4ControllerPort = 1;
 
         public static final int kLeftXAxis = 0;
         public static final int kLeftYAxis = 1;
-        public static final int kRightXAxis = 2;
+        public static final int kRightXAxis = 4;
 
         public static final int leftBumper = 5;
         public static final int rightBumper = 6;
