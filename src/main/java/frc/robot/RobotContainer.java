a3afd61c38a07c1f6d708c48df3d65db8c8a6353
Krishna <callkris26@gmail.com>
2023-02-12 14:37:26 -0500

    Added Auton test

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,12 +4,26 @@
 
 package frc.robot;
 
+import java.util.List;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.Constants.AutoConstants;
+import frc.robot.Constants.DriveConstants;
 import frc.robot.Constants.OIConstants;
 import frc.robot.commands.AlignToAprilTag;
 import frc.robot.commands.StraightenRobotCmd;
@@ -44,6 +58,44 @@ public class RobotContainer {
   }
 
   public Command getAutonomousCommand() {
-    return new AlignToAprilTag(limelight, swerveSubsystem);
+           //1. Sets the configuration of the trajectory config with the max speed and acceleration the robot could go. Then sets the configuration to the kinematics of swerve drive.
+           TrajectoryConfig trajectoryConfig = new TrajectoryConfig(Constants.AutoConstants.kAutoMaxSpeedMetersPerSecond, 
+           Constants.AutoConstants.kAutoMaxAccelerationMetersPerSecondSquared).setKinematics(Constants.DriveConstants.kDriveKinematics);
+   
+            // 2. Generate trajectory
+            Trajectory trajectory = TrajectoryGenerator.generateTrajectory(
+             new Pose2d(0, 0, new Rotation2d(0)),
+             List.of(
+                     new Translation2d(1, 0),
+                     new Translation2d(2, 0)
+                     ),
+             new Pose2d(0, 0, new Rotation2d(0)),
+             trajectoryConfig);
+   
+           // 3. Define PID controllers for tracking trajectory
+           PIDController xController = new PIDController(AutoConstants.kPXController, 0, 0);
+           PIDController yController = new PIDController(AutoConstants.kPYController, 0, 0);
+           ProfiledPIDController thetaController = new ProfiledPIDController(
+                   AutoConstants.kPThetaController, 0, 0, AutoConstants.kThetaControllerConstraints);
+           thetaController.enableContinuousInput(-Math.PI, Math.PI);
+   
+           // 4. Construct command to follow trajectory
+           SwerveControllerCommand swerveControllerCommand = new SwerveControllerCommand(
+                   trajectory,
+                   swerveSubsystem::getPose,
+                   DriveConstants.kDriveKinematics,
+                   xController,
+                   yController,
+                   thetaController,
+                   swerveSubsystem::setModuleStates,
+                   swerveSubsystem);
+    
+           
+   
+           // 5. Add some init and wrap-up, and return everything
+           return new SequentialCommandGroup(
+                   new InstantCommand(() -> swerveSubsystem.resetOdometry(trajectory.getInitialPose())),
+                   swerveControllerCommand,
+                   new InstantCommand(() -> swerveSubsystem.stopModules()));
   }
 }
