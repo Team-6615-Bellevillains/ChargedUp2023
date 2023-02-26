e4de52b916ed391e0fa5a38f4fcb52a332ade997
978-9-17-637879-3 <102838865+978-9-17-637879-3@users.noreply.github.com>
2023-02-26 14:18:39 -0500

    Fix Grabber, disable pressure commands

---
+++ src/main/java/frc/robot/commands/grabber/ManualGrabberFlipInCmd.java
@@ -0,0 +1,36 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.grabber;
+
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
+import frc.robot.subsystems.GrabberSubsystem;
+
+public class ManualGrabberFlipInCmd extends CommandBase {
+    private GrabberSubsystem grabberSubsystem;
+
+    public ManualGrabberFlipInCmd(GrabberSubsystem grabberSubsystem) {
+        this.grabberSubsystem = grabberSubsystem;
+
+        addRequirements(grabberSubsystem);
+    }
+
+    @Override
+    public void execute() {
+        grabberSubsystem.setFlipMotorSpeed(-.4);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        grabberSubsystem.setFlipMotorSpeed(0);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return Math.abs(grabberSubsystem.getFlipEncoderPosition() - Constants.GrabberConstants.grabberInSetpoint) < Units.degreesToRadians(10);
+    }
+
+}
