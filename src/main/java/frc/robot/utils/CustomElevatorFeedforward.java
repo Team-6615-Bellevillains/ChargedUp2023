b5d38d59693f966f47c484198c7ac01e2bad8374
978-9-17-637879-3 <102838865+978-9-17-637879-3@users.noreply.github.com>
2023-02-26 18:06:57 -0500

    Remove CustomElevatorFeedforward, switch to wpilib ElevatorFeedforward

--- src/main/java/frc/robot/utils/CustomElevatorFeedforward.java
+++
@@ -1,9 +0,0 @@
-package frc.robot.utils;
-
-import frc.robot.Constants.ElevatorConstants;
-
-public class CustomElevatorFeedforward {
-    public static double feedforward(double velocity) {
-        return ElevatorConstants.kGVerticalElevator + ElevatorConstants.kSVerticalElevator * Math.signum(velocity) + ElevatorConstants.kVVerticalElevator * velocity;
-    }
-}
